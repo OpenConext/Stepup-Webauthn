e20c743bc9501c3a93950a5a5804d51261940847
Michiel Kodde <mkodde@ibuildings.nl>
2020-09-15 15:22:28 +0200

    Satisfy PublicKeyCredential interface requirements
    The getClientExtensionResults function was missing on the
    serializePublicKeyCredential function. Adding it resolved breaking TS
    build

--- public/typescript/serialisation/SerializedPublicKeyCredential.ts
+++ public/typescript/serialisation/SerializedPublicKeyCredential.ts
@@ -3,10 +3,12 @@ import { serializeAuthenticatorResponse } from './AuthenticatorAttestationRespon
 import { uInt8ToBase64UrlSafe } from './common';
 
 export const serializePublicKeyCredential: (credentials: PublicKeyCredential) => SerializedPublicKeyCredential =
-  ({ id, rawId, response, type }) =>
-    ({
+  ({ id, rawId, response, type,  getClientExtensionResults }) => {
+    return ({
       id,
       rawId: uInt8ToBase64UrlSafe(rawId),
       response: serializeAuthenticatorResponse(response),
       type,
+      getClientExtensionResults,
     });
+  };
