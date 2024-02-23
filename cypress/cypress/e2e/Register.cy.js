0606df60b494278599f407efeff84843c169a5fc
KarsanHAM <karshamelink97@live.nl>
2024-02-23 10:47:59 +0100

    Disable Auth UI for automated testing

--- cypress/cypress/e2e/Register.cy.js
+++ cypress/cypress/e2e/Register.cy.js
@@ -1,8 +1,9 @@
 describe('template spec', () => {
+  //implements https://chromedevtools.github.io/devtools-protocol/tot/WebAuthn/
   function addVirtualAuthenticator () {
     return Cypress.automation("remote:debugger:protocol", {
       command: "WebAuthn.enable",
-      params: {"enableUI":true},
+      params: {},
     }).then((result) => {
       console.log("WebAuthn.enable", result);
       return Cypress.automation("remote:debugger:protocol", {
@@ -14,6 +15,7 @@ describe('template spec', () => {
             hasResidentKey: true,
             hasUserVerification: true,
             isUserVerified: true,
+            automaticPresenceSimulation: true,
           },
         },
       }).then((result) => {
@@ -24,17 +26,12 @@ describe('template spec', () => {
   }
 
   it('Registers a FIDO2 token using the Demo SP', () => {
-    //Set the document.domain to "webauthn.dev.openconext.local", the virtual authenticator reads the document.domain and sets the RPID accordingly,
-    //if the non-https version is not visited first the authenticator will use "openconext.local" as an RPID
-    //this consequently leads to an RPIDhash missmatch with the sever RPID.(https://github.com/cypress-io/cypress/issues/2193)
+
     cy.visit('https://webauthn.dev.openconext.local/')
-    // console.log('domain when starting = ' + document.domain)
-    // const domain = document.location.href.match(/\/\/([^:/]+)/)[1]
-    // document.domain = domain
-    // console.log('domain after modifying = ' + document.domain)
-    // cy.document().its(domain).should('eq', 'webauthn.dev.openconext.local')
     addVirtualAuthenticator()
 
-    cy.contains('Registreer gebruiker').click()
+    // cy.contains('Registreer gebruiker').click()
+    //
+    // cy.contains('Demo service provider ACS')
   })
 })
