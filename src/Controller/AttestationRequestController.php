622fd235869ef02c6b82e9071d40bfd893746dcf
KarsanHAM <karshamelink97@live.nl>
2024-03-08 09:38:28 +0100

    Clean up AttestationResponseController.php

--- src/Controller/AttestationRequestController.php
+++ src/Controller/AttestationRequestController.php
@@ -24,19 +24,19 @@ use Surfnet\Webauthn\PublicKeyCredentialCreationOptionsStore;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
-use Throwable;
 use Webauthn\Bundle\Security\Handler\DefaultCreationOptionsHandler;
 
 final readonly class AttestationRequestController
 {
     public function __construct(
-        private PublicKeyCredentialCreationOptionsStore              $creationOptionsStore,
-        private DefaultCreationOptionsHandler                        $creationOptionsHandler
+        private PublicKeyCredentialCreationOptionsStore $creationOptionsStore,
+        private DefaultCreationOptionsHandler $creationOptionsHandler
     ) {
     }
 
+    /**
+     * Handles the attestation public key request.
+     */
     #[Route(path: '/attestation-options', name: 'attestation-options', methods: ['POST'])]
     public function action(Request $request): Response
     {
